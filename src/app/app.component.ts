3fc08cb03c50f57120e3b4c41da9bc8318b038cc
Jose Iriarte <josemariairiarte@gmail>
2024-01-08 18:52:50 -0300

    Angular Fundamental Concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,29 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  // name of the tag we use in the index.html file
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
+  // the component contents - the view - is here:
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+// this would be the model/data:
 export class AppComponent {
-  title = 'angular-course';
+  title = 'Angular Core Deep Dive';
+  data = {
+    title: 'This is a title also'
+  }
+  onLogoClicked() {
+    alert('Hello World!');
+  }
+  // so here the onKeyUp function receives
+  // var newTitle and modifies the model
+  // object data.title (on all elements that use it)
+  // from 'This is a title also'
+  // to the title on the input object named #inputtitle
+  onKeyUp(newTitle:string){
+    this.data.title = newTitle;
+  }
 }
